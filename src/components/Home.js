9265a87060a7a5fa71802f725def3f5421edeef5
Kratious <semjinghan@gmail.com>
2019-06-06 00:33:04 +0800

    Add initial design of home page
    Inluded some placeholder images as well.
    The design of featured categories section still needs work to
    include category names and make the entire column clickable links.
    
    Also still have to add footer component for the page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,52 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
+import NavBar from './Nav';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hi there! I am the Home Page.</h1>
+        <NavBar />
+        <div className="ui inverted vertical masthead center aligned segment">
+          <div className="ui text container">
+            <h1 className="ui inverted header">
+              TRADE
+            </h1>
+            <h2>The best quality products, available to everyone.</h2>
+            <div className="ui fluid icon input">
+              <input type="text" placeholder="Find your items" />
+              <i className="search icon"></i>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="ui vertical stripe segment">
+          <div className="ui middle aligned stackable grid container">
+            <div className="row">
+              <h1 className="ui header column">Featured Categories</h1>
+            </div>
+            <div className="row featured-categories">
+              <div className="four wide column">
+                <img src="/images/fashion.jpg" className="ui large bordered image" />
+              </div>
+              <div className="four wide column">
+                <img src="/images/electronics.jpg" className="ui large bordered image" />
+              </div>
+              <div className="four wide column">
+                <img src="/images/essentials.jpg" className="ui large bordered image" />
+              </div>
+              <div className="four wide center aligned column">
+                <h1 className="ui header" >Shop Now</h1>
+              </div>
+            </div>
+
+          </div>
+                   
+
+        </div>
+       
+        
       </div>
     );
   };
