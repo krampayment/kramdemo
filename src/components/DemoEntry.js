009ad95685d298ca24afca96c11da44703ef28b4
tanchc <tanchc16@gmail.com>
2019-06-18 02:56:51 -0700

    Changed server url

--- src/components/DemoEntry.js
+++ src/components/DemoEntry.js
@@ -10,7 +10,7 @@ class DemoEntry extends React.Component {
   }
 
   sendEmail = () => {
-    axios.post("http://localhost:9000/", {
+    axios.post("http://demo.krampayment.com:9000/", {
       members: [{
         "email_address": this.state.email,
         "status": "unsubscribed",
